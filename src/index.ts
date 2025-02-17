ac386924a95d25d66bf955eab5f0db9b0c6b1937
Cameron Tacklind <cameron@tacklind.com>
2025-02-16 17:44:33 -0800

    Use functions

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,10 @@ const autoClose = false;
 const userDataDir = 'user-data';
 
 function areDatesClose(date1: string | Date, date2: string | Date, days = 4) {
-  const a = new Date(date1);
-  const b = new Date(date2);
-
   const maxTimeDelta = 1000 * 60 * 60 * 24 * days;
+  const delta = absTimeDelta(date1, date2);
 
-  return Math.abs(a.getTime() - b.getTime()) < maxTimeDelta;
+  return delta < maxTimeDelta;
 }
 
 function negate(amount: string) {
