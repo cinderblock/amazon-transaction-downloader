c9b3863debfa5ef93a1d89efb31c2e8a49669517
Cameron Tacklind <cameron@tacklind.com>
2025-02-18 14:04:11 -0800

    Support Digital orders

--- src/AmazonConstants.ts
+++ src/AmazonConstants.ts
@@ -1,2 +1,2 @@
-export const OrderRegex = /^\d{3}-\d{7}-\d{7}$/;
+export const OrderRegex = /^[\dD]\d{2}-\d{7}-\d{7}$/;
 export const TransactionUrl = 'https://www.amazon.com/cpe/yourpayments/transactions';
